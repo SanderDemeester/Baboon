7388356830d1d9d525d868ce58ebcd9b523737b1
Sander Demeester <demeester.sander@gmail.com>
2012-05-18 13:33:46 +0200

    concat string + print debug output

--- unsecure_server/filegraph.c
+++ unsecure_server/filegraph.c
@@ -15,23 +15,19 @@ int construct_graph(char *root){
   root_d = opendir(root);
   if(root_d != NULL){
     while(listing = readdir(root_d)){
-      char *path_file = malloc(strlen(root) + strlen(listing->d_name) + 2);
+      char *path_file = malloc(strlen(root) + strlen(listing->d_name) + 1);
       memcpy(path_file,root,strlen(root));
-      path_file[strlen(root)] = ' ';
-      memcpy(path_file + strlen(root) + 1, listing->d_name,strlen(listing->d_name)+1);
+      memcpy(path_file + strlen(root), listing->d_name,strlen(listing->d_name)+1);
       FILE *file = fopen(path_file,"rb");
       char line[100] = {0};
       if(file != NULL){
 	while(fscanf(file,"%s",&line) == 1){
 	  printf("%s \n",line);
-	  
 	}
       }else{
 	printf("fout\n");
       }
-      puts(listing->d_name);}
-	
-    
+    }
     (void)closedir(root_d);
   }
 }
