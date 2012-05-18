4b9dc9c047f1a18c5f8036750a3b238d4ab01f18
Sander Demeester <demeester.sander@gmail.com>
2012-05-18 13:29:15 +0200

    string concat to get full path-name

--- unsecure_server/filegraph.c
+++ unsecure_server/filegraph.c
@@ -14,8 +14,24 @@ int construct_graph(char *root){
   struct dirent *listing;
   root_d = opendir(root);
   if(root_d != NULL){
-    while(listing = readdir(root_d))
-            puts(listing->d_name);
+    while(listing = readdir(root_d)){
+      char *path_file = malloc(strlen(root) + strlen(listing->d_name) + 2);
+      memcpy(path_file,root,strlen(root));
+      path_file[strlen(root)] = ' ';
+      memcpy(path_file + strlen(root) + 1, listing->d_name,strlen(listing->d_name)+1);
+      FILE *file = fopen(path_file,"rb");
+      char line[100] = {0};
+      if(file != NULL){
+	while(fscanf(file,"%s",&line) == 1){
+	  printf("%s \n",line);
+	  
+	}
+      }else{
+	printf("fout\n");
+      }
+      puts(listing->d_name);}
+	
+    
     (void)closedir(root_d);
   }
 }
