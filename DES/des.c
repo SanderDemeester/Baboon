0e7dff67791dbc849c8eb74cae7ab183fdd80d82
Sander Demeester <demeester.sander@gmail.com>
2012-06-23 18:08:14 +0200

    added more comments + brief text on the feistel function

--- DES/des.c
+++ DES/des.c
@@ -4,12 +4,38 @@
 #define SET_BIT (array,bit)   \ (array[(int) (bit / 8)] |=  ( 0x80 >> (bit)))
 #define CLEAR_BIT (array,bit) \ (array[(int) (bit / 8)] &= ~( 0x80 >> (bit)))
 
+/********************************/
+/* DES inital permutation table */
+/********************************/
+
+static const int ip_table[] = {
+  58,50,42,34,26,18,10,2,
+  60,52,44,36,26,20,12,4,
+  62,54,46,38,30,22,14,6,
+  64,56,48,40,32,24,16,8,
+  57,49,41,33,25,17,9,1,
+  59,51,43,35,27,19,11,3,
+  61,53,45,37,29,21,13,5,
+  63,55,47,39,31,23,25,7};
+
+/*********************************************************************/
+/* Overwirtes the target array with the XOR of it and the src array. */
+/*********************************************************************/
+
 static void xor(unsigned char *target, const unsigned char *source, int len){
   while(len--){
     *target++ ^= *src++;
   }
 }
 
+/********************************************************************/
+/* Depending on the permute_table, this is the "<3" of our DES code.
+/* permute table SHOULD have exactly les and les * 8 number of entries.
+/* copy the second bit of the last byte into the first bit of the first byte of the output.
+/* the first byte of output comes from the second bits of each input byte.
+/* the second byte of output comes rom the fouth bits of each input byte
+/* You see where this is goning..
+/********************************************************************/
 static void permute(unsigned char target[], const unsigned char src[], const int parmute_table[], int len){
   int i;
   for(i = 0; i < len * 8; i++){
@@ -17,5 +43,6 @@ static void permute(unsigned char target[], const unsigned char src[], const int
       SET_BIT(target,i);
     else
       CLEAR_BIT(target,i);
+  }
   }
 }
