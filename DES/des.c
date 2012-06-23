2f4db2aa16c435235193e0fc8e4895edd26cd05e
Sander Demeester <sanderd@zeus.ugent.be>
2012-06-23 13:11:04 +0200

    xor functie

--- DES/des.c
+++ DES/des.c
@@ -3,3 +3,9 @@
 #define GET_BIT (array,bit)   \ (array[(int) (bit / 8)] &   ( 0x80 >> (bit)))
 #define SET_BIT (array,bit)   \ (array[(int) (bit / 8)] |=  ( 0x80 >> (bit)))
 #define CLEAR_BIT (array,bit) \ (array[(int) (bit / 8)] &= ~( 0x80 >> (bit)))
+
+static void xor(unsigned char *target, const unsigned char *source, int len){
+  while(len--){
+    *target++ ^= *src++;
+  }
+}
