a5e16e0cc3a8a4b72c3d8e6ce2a9c5c8b1c2c2a2
Sander Demeester <sanderd@zeus.ugent.be>
2012-06-23 13:17:48 +0200

    de DES permutatie functie (waar ben ik in godsnaam aan begonnen).

--- DES/des.c
+++ DES/des.c
@@ -9,3 +9,13 @@ static void xor(unsigned char *target, const unsigned char *source, int len){
     *target++ ^= *src++;
   }
 }
+
+static void permute(unsigned char target[], const unsigned char src[], const int parmute_table[], int len){
+  int i;
+  for(i = 0; i < len * 8; i++){
+    if(GET_BIT(src,(permute_table[i]-1)))
+      SET_BIT(target,i);
+    else
+      CLEAR_BIT(target,i);
+  }
+}
