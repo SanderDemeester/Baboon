a4d92c027c2ba0e3e2887308bfa6d871bb9b8a81
Sander Demeester <demeester.sander@gmail.com>
2012-06-24 10:53:45 +0200

    fixed bug in subkey rotation

--- header/des.h
+++ header/des.h
@@ -4,7 +4,7 @@
 /***********/
 /* MACRO'S */
 /***********/
-#define GET_BIT(array, bit)   (array[(int) (bit/8)] & (0x80 >> (bit % 8)))
+#define GET_BIT(array, bit)   (array[(int) (bit / 8)] & (0x80 >> (bit % 8)))
 #define SET_BIT(array, bit)   (array[(int) (bit / 8)] |=  (0x80 >> (bit % 8)))
 #define CLEAR_BIT(array, bit) (array[(int) (bit / 8)] &= ~(0x80 >> (bit % 8)))
 
