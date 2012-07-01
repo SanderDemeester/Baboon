f9c6a60c4e5d3052acf384755985c46229ab572d
Sander Demeester <demeester.sander@gmail.com>
2012-07-01 17:21:07 +0200

    Performing subsitution.

--- aes/aes.c
+++ aes/aes.c
@@ -19,3 +19,15 @@ static void rotate_word(unsigned char *w){
   w[2] = w[3];
   w[3] = temp;
 }
+
+/**************************/
+/* replace with AES sbox */
+/**************************/
+static void subsitute_word(unsigned char *w){
+  
+  int i = 0;
+  for(;i < 4; i++){
+    w[i] = sbox[(w[i] & 0xF0) << 4][w[i] & 0x0F]; //calculate offset with ORing 0xF0 to jump get the pos.
+  }
+  
+}
