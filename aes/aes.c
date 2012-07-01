8a2a128fd3b00f59c9186500f032474f5abbf8f6
Sander Demeester <demeester.sander@gmail.com>
2012-07-01 17:36:43 +0200

    This needs some testing code. I think there are the right positions for the row shift operation.

--- aes/aes.c
+++ aes/aes.c
@@ -67,3 +67,32 @@ static void subsitute_bytes(unsigned char state[][4]){
     }
   }
 }
+
+
+/*****************/
+/* AES row shift */
+/*****************/
+static void shift_rows(unsigned char state[][4]){
+  
+  int temp;
+  temp = state[1][0];
+
+  state[1][0] = state[1][1];
+  state[1][1] = state[1][2];
+  state[1][2] = state[1][3];
+  state[1][3] = temp;
+
+  temp = state[2][0];
+  state[2][0] = state[2][2];
+  state[2][2] = temp;
+
+  temp = state[2][1];
+  state[2][1] = state[2][3];
+  state[2][3] = temp;
+
+  temp=state[3][3];
+  state[3][3] = state[3][2];
+  state[3][2] = state[3][1];
+  state[3][1] = state[3][0];
+  state[3][0] = temp;
+}
