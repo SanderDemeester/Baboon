21b07aa439700a80613814607c1408b90f6ae1ce
Sander Demeester <demeester.sander@gmail.com>
2012-07-01 16:53:59 +0200

    added all AES header signature. Let's start up our "algebra brain-part".

--- header/aes.h
+++ header/aes.h
@@ -106,3 +106,38 @@ static void aes_decrypt(const unsigned char *input,
 			const unsigned char *iv,
 			const unsigned char *key,
 			int key_lengte);
+
+/*************************************/
+/* AES final 128 encryption function */
+/*************************************/
+void aes_128_encrypt(const unsigned char *plaintext,
+		     const int plaintext_len,
+		     unsigned char ciphertext[],
+		     const unsigned char *iv,
+		     const unsigned char *key);
+
+/*************************************/
+/* AES final 128 decryption function */
+/*************************************/
+void aes_128_decrypt(const unsigned char *ciphertext,
+		     unsigned char plaintext[],
+		     const unsigned char *iv,
+		     const unsigned char *key);
+
+/****************************/
+/* AES final 256 encryption */
+/****************************/
+void aes_256_encrypt(const unsigned char *plaintext,
+		     const int plaintext_len,
+		     unsigned char ciphertext[],
+		     const unsigned char *iv,
+		     const unsigned char *key);
+
+/*************************************/
+/* AES final 256 decryption function */
+/*************************************/
+void aes_256_decrypt(const unsigned car *ciphertext,
+		     const int ciphertext_len,
+		     const unsigned char *iv,
+		     const unsigned char *key);
+		     
