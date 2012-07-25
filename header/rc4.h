d81532037d9fd9fddd9208e1eec58cd9b9dbd61d
Sander Demeester <demeester.sander@gmail.com>
2012-07-25 10:07:21 +0200

    added length wrappers implementation

--- header/rc4.h
+++ header/rc4.h
@@ -23,6 +23,19 @@ void rc4_40_decrypt(const unsigned char *ciphertext,
 		    void *state,
 		    const unsigned char *key);
 
+void rc4_128_encrypt(const unsigned char *plaintext, 
+		     const int plaintext_len,
+		     unsigned char ciphertext[],
+		     void *state,
+		     const unsigned char *key);
+
+void rc4_128_decrypt(const unsigned char *ciphertext,
+		     const int ciphertext_len,
+		     unsigned char plaintext[],
+		     void *state,
+		     const unsigned char *key);
+		     
+
 typedef struct { 
   int i;
   int j;
--- src/cryptografic_algoritmes/rc4/rc4.c
+++ src/cryptografic_algoritmes/rc4/rc4.c
@@ -62,3 +62,21 @@ void rc4_40_decrypt(const unsigned char *ciphertext,
   rc4_operate(ciphertext, ciphertext_len, plaintext, key, 5, 
 	      (rc4_state *) state);
 }
+
+void rc4_128_encrypt(const unsigned char *plaintext, 
+		     const int plaintext_len,
+		     unsigned char ciphertext[],
+		     void *state,
+		     const unsigned char *key){
+  rc4_operate(plaintext, plaintext_len, ciphertext, key, 16,
+	      (rc4_state *) state);
+}
+
+void rc4_128_decrypt(const unsigned char *ciphertext,
+		     const int ciphertext_len,
+		     unsigned char plaintext[],
+		     void *state,
+		     const unsigned char *key){
+  rc4_operate(ciphertext, ciphertext_len, plaintext, key, 16,
+	      (rc4_operate *) state);
+}
