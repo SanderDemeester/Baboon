af69eb6f4a7e4733c41cda0aa5f299c177a7ebae
Sander Demeester <demeester.sander@gmail.com>
2012-07-25 09:28:50 +0200

    Added rc4 state struct in rc4 header file

--- header/rc4.h
+++ header/rc4.h
@@ -1,6 +1,12 @@
 #define _RC4_H
+#define RC4_STATE_ARRAY_LEN 256
+
 void rc4_operate(const unsigned char *plaintext,
 			int plaintext_len,
 			unsigned char *cijfertext,
 			unsigned char *key,
 			int key_len);
+typedef struct { 
+  int i;
+  int j;
+}rc4_state;
