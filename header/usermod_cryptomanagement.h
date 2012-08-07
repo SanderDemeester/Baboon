6779ff0dbc102dbf7d07496a711ec936f4e11733
Sander Demeester <demeester.sander@gmail.com>
2012-08-07 10:34:28 +0200

    added signature usermode crytpo functions

---
+++ header/usermod_cryptomanagement.h
@@ -0,0 +1,5 @@
+#define _USERMODE_CRYPTO_MANAGEMENT
+void usermode_aes(int argc, char** argv);
+void usermode_rc4(int argc, char** argv);
+void usermode_des(int argc, char** argv);
+void usermode_3des(int argc, char** argv);
