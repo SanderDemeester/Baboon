01f44a11d81bcb771fbf61c632ff74a9291adf53
Sander Demeester <demeester.sander@gmail.com>
2012-09-28 18:48:51 +0200

    when exersising only the cryptografic functions of baboon, it not nessecary that after it is done it starts in webserver mode

--- src/unsecure_server/arguments.c
+++ src/unsecure_server/arguments.c
@@ -118,6 +118,7 @@ void parse_arguments(int argc, char *argv[], struct arguments *arg_){
     for(option_index = 0; option_index < CRYPTO_OPTIONS-1; option_index++){
       if(crypto_handlers[option_index]->value == uniq_functionpointer_identifier){
     	crypto_handlers[option_index]->function_pointer(argc,argv);
+	exit(0); //stop executing proces
       }
     }
   }
