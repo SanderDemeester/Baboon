f5dbd9e75f14739945bce9d20cf9b3eba1b20d82
Sander Demeester <demeester.sander@gmail.com>
2012-09-30 23:06:55 +0200

    added return statement

--- src/unsecure_server/filegraph.c
+++ src/unsecure_server/filegraph.c
@@ -126,7 +126,7 @@ int dir_re(const char *path, context_unit *filestructure_start){
        construct_dependency(path_file,listing,filestructure_start); 
     }
   }
-  closedir(root_d);
+  return closedir(root_d);
 }
 
 int construct_graph(char *root){
